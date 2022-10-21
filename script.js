f6ae269531f3b1e2a67421d299a9ead713922988
QuavoIsMyCat <Kcodell30@gmail.com>
2022-10-21 16:50:17 -0500

    created initial grid for project

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 256; i++) {
+	const container = document.querySelector("#container");
+
+	const content = document.createElement("div");
+	content.classList.add("square");
+
+	container.appendChild(content);
+}
